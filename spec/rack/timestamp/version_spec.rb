b699c8dfde1062d6f7b19cd7ae8321afa949030b
Maxime Chaisse-Leal <maxime.chaisseleal@gmail.com>
2016-08-27 21:17:25 -0700

    version test should be wrapped within a describe

--- spec/rack/timestamp/version_spec.rb
+++ spec/rack/timestamp/version_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Rack::Timestamp do
-  it { expect(described_class::VERSION).not_to be nil }
+
+  describe 'version' do
+    it { expect(described_class::VERSION).not_to be nil }
+  end
 end
